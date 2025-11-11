a3f1bc527cee26e200408d878b8aa3454e4d4085
GorkaFerreiro <gorka.ferreiro@maristak.net>
2025-11-11 08:41:09 +0100

    Add files via upload

---
+++ RETO3/volumes/apache-html/login.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <form action="auth.php" method="post">
+    Usuario: <input type="text" name="username"><br>
+    Contraseña: <input type="password" name="password"><br>
+    <input type="submit" value="Entrar">
+  </form>
+</body>
+</html>
